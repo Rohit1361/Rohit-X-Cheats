27c6c7081eb4077bfbf3fc15adb2585845f3e81d
Rohit Swami <rohitswami1361@gmail.com>
2025-02-15 16:11:49 +0530

    setup

--- WinFormsApp1/WinFormsApp1/obj/Release/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/WinFormsApp1/obj/Release/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a3e226ffa85e4f7e94027f3925b30834072c16d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
