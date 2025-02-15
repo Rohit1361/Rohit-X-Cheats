37da7d717758b397d34403873d8fa5e71c066fd4
Rohit Swami <rohitswami1361@gmail.com>
2025-02-15 21:14:48 +0530

    update1

--- WinFormsApp1/WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -39,7 +39,7 @@
             label1.Name = "label1";
             label1.Size = new Size(128, 37);
             label1.TabIndex = 0;
-            label1.Text = "Version1";
+            label1.Text = "Version2";
             // 
             // Form1
             // 
